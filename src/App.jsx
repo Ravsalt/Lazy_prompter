2c22d6fb14ebbac85a0e9018e009c2f335390589
ravsalt <ravpog10@gmail.com>
2025-06-25 12:18:22 +0800

    feat: add Vercel analytics and speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Layout from './components/Layout/Layout';
 import PromptGenerator from './components/PromptGenerator/PromptGenerator';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
         <PromptGenerator />
       </div>
+      <Analytics />
+      <SpeedInsights />
     </Layout>
   );
 }
